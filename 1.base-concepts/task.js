48c1b09e2a48393177596e8130069bae4bede48a
Marina Shalamova <tusya120@mail.ru>
2022-08-11 20:03:05 +0300

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,54 @@
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+  arr = [];
+  let d = b ** 2 - 4 * a * c;
+  if (d > 0) {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+  } else if (d === 0) {
+    arr.push(-b / (2 * a));
+  }
+
   return arr; // array
-}
+
+};
+
+console.log(solveEquation(1, 5, 4));
+
+
+
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
-  // код для задачи №2 писать здесь
+  if (isNaN(percent)) {
+    return totalAmount = `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  } else if (isNaN(contribution)) {
+    return totalAmount = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  } else if (isNaN(amount)) {
+    return totalAmount = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+  } else {
 
-  return totalAmount;
+    // количество месяцев для кредита
+    let numberOfMonth = Math.trunc((Date.parse(date) - Date.now()) / (1000 * 60 * 60 * 24 * 30));
+
+    // тело кредита
+    let s = amount - contribution;
+
+    // процентная ставка
+    let percentPart = percent / 12 / 100;
+
+    // ежемесячная оплата
+    let totalAmountMonth = s * (percentPart + (percentPart / (((1 + percentPart) ** numberOfMonth) - 1)));
+
+    // общий платёж
+    totalAmount = (totalAmountMonth * numberOfMonth).toFixed(2);
+
+    return + totalAmount;
+  }
+
 }
+
