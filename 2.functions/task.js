23fce8832c3abde1af307f568a77a1ae60c7e839
Marina Shalamova <tusya120@mail.ru>
2022-08-16 18:59:51 +0300

    first commit variables

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,62 @@
-// Задание 1
+// // Задание 1
+arr1 = [];
+for (let i = -100; i < 101; i++) {
+  arr1.push(i);
+}
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
-
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg;
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    };
+    if (arr[i] < min) {
+      min = arr[i];
+    };
+    sum += arr[i];
+    avg = + ((sum / arr.length).toFixed(2));
+  }
   return { min: min, max: max, avg: avg };
 }
+// console.log(getArrayParams([1, 2, 2]));
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  };
   return sum;
-}
-
+};
+// console.log(worker([1, 15, 5]));
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = 0;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let result = func(arrOfArr[i]);
+    if (result > max) {
+      max = result;
+    };
+  };
   return max;
-}
+};
+console.log(makeWork([[10, 10, 11], [20, 10]], worker2));
 
 // Задание 3
 function worker2(arr) {
   // Ваш код
+  let min = Infinity;
+  let max = -Infinity;
+  let diff;
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    };
+    if (arr[i] < min) {
+      min = arr[i];
+    };
+    diff = Math.abs(max - min);
+  };
+  return diff;
 }
