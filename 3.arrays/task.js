0a110d50f7aaeb3ceffff30b7a6b70be0516dda8
Marina Shalamova <tusya120@mail.ru>
2022-08-20 17:40:29 +0300

    first commit varia

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,11 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
+  let result = arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
   return result; // boolean
-}
+};
+// console.log(compareArrays([10, 20, -3], [10, 20, -3]))
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr.filter(element => element > 0).filter(element => element % 3 === 0).map((element) => element * 10);;
   return resultArr; // array
-}
+};
+// console.log(advancedFilter([4,3,5]))
