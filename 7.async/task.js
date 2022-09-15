707b959d2073e9dc25485de3f5c31ed46284c06d
Marina Shalamova <tusya120@mail.ru>
2022-09-15 18:48:25 +0300

    first commit variables

---
+++ 7.async/task.js
@@ -0,0 +1,141 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.timerId = null;
+    };
+
+    addClock(time, callback, id) {
+        let idEment = element => element.id === id;
+        if (!id) {
+            throw new Error('error text');
+        };
+
+        if (this.alarmCollection.some(idEment)) {
+            console.error("Звонок существует");
+            return;
+        };
+        this.alarmCollection.push({ id, time, callback });
+    };
+
+    removeClock(id) {
+        const initialLength = this.alarmCollection.length;
+        this.alarmCollection = this.alarmCollection.filter(element => element.id !== id);
+        return this.alarmCollection.length !== initialLength;
+    };
+
+    getCurrentFormattedTime(currentDate) {
+        currentDate = new Date();
+        return (`${currentDate.getHours()}:${currentDate.getMinutes()}`);
+    };
+
+    start() {
+        if (this.timerId) {
+            return;
+        };
+
+        this.timerId = setInterval(() => {
+            this.alarmCollection.forEach(theBell => {
+                if (theBell.time === this.getCurrentFormattedTime()) {
+                    theBell.callback();
+                }
+            })
+        }, 1000);
+    };
+
+    stop() {
+        if (this.timerId) {
+            clearInterval(this.timerId);
+            this.timerId = null;
+        };
+    };
+
+    printAlarms() {
+        this.alarmCollection.forEach((item) => console.log(item.id + "," + item.time));
+        return;
+    };
+
+    clearAlarms() {
+        this.stop();
+        this.alarmCollection = [];
+    };
+
+
+};
+
+function testCase() {
+    let phoneAlarm = new AlarmClock();
+    phoneAlarm.addClock('15:03', () => console.log('Пора вставать'), 1);
+    phoneAlarm.addClock('15:04', () => { console.log('Пора вставать прямо сейчас'), phoneAlarm.removeClock(2) }, 2);
+    phoneAlarm.addClock('15:04', () => console.log('Вставай уже'));
+    phoneAlarm.addClock('15:05', () => {
+        console.log('Вставай уже, а то проспишь'),
+            phoneAlarm.clearAlarms();
+        phoneAlarm.printAlarms();
+    }, 3);
+    phoneAlarm.addClock('15:06', () => console.log('ПОДЪЁМ!!!'), 1);
+    phoneAlarm.printAlarms();
+    phoneAlarm.start();
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function testCase() {
+// const alarm  = new AlarmClock();
+// console.log(alarm);
+// this.addClock(getCurrentFormattedTime, printAlarms(), 1)
+
+// }
+// testCase()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,91 @@
-function cachingDecoratorNew(func) {
-  // Ваш код
+// function cachingDecoratorNew(func) {
+//   // Ваш код
+//   let cache = [];
+//   return function wrapper(...arg) {
+//     const hash = arg.join(",");
+//     let objectInCache = cache.find((item) => item.hash === hash);
+//     if (objectInCache) {
+//       console.log("Из кэша:" + objectInCache.value);
+//       console.log(cache);
+//       return "Из кэша: " + objectInCache.value;
+//     }
+
+//     const result = func(...arg)
+//     cache.push({ hash: hash, value: result });
+//     if (cache.length > 5) {
+//       cache.shift();
+//     }
+//     console.log("Вычисляем: " + result);
+//     console.log(cache);
+//     return "Вычисляем: " + result;
+//   }
+// }
+
+// const addThree = (a, b, c) => a + b + c;
+// const upgradedAddThree = cachingDecoratorNew(addThree);
+// upgradedAddThree(1, 2, 3); // вычисляем: 6
+// upgradedAddThree(1, 2, 3); // из кэша: 6
+// upgradedAddThree(2, 2, 3); // вычисляем: 7
+// upgradedAddThree(3, 2, 3); // вычисляем: 8
+// upgradedAddThree(4, 2, 3); // вычисляем: 9
+// upgradedAddThree(5, 2, 3); // вычисляем: 10
+// upgradedAddThree(6, 2, 3); // вычисляем: 11 (при этом кэш для 1, 2, 3 уничтожается)
+// upgradedAddThree(1, 2, 3); // вычисляем: 6  (снова вычисляем, кэша нет)
+// upgradedAddThree(1, 2, 3);
+
+
+debounceDecoratorNew.allCount = 0
+this.wrapper.allCount = 0;
+function debounceDecoratorNew(func, delay) {
+   let timeoutId = null;
+  return function wrapper(...args) {
+    console.log(wrapper.allCount++);
+    console.log("вызвали декорированную функцию")
+    if (!timeoutId) {
+      func(...args);
+    };
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      setTimeout.count++;
+      func(...args);
+    }, delay);
+    // setTimeout.count = 1;
+    // return setTimeout.count
+  }
+ 
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
-}
+console.log(wrapper.allCount)
 
-function debounceDecorator2(func) {
-  // Ваш код
-}
+
+
+const sendSignal = (signalOrder, delay) => console.log("Сигнал отправлен", signalOrder, delay);
+const upgradedSendSignal = debounceDecoratorNew(sendSignal, 2000);
+
+setTimeout(() => upgradedSendSignal(1, 0));
+// Сигнал отправлен + будет запланирован асинхронный запуск, который будет проигнорирован так как следующий сигнал отменит предыдущий (300 - 0 < 2000)
+setTimeout(() => upgradedSendSignal(2, 300), 300);
+// проигнорировано так как следующий сигнал отменит предыдущий (900 - 300 < 2000)
+setTimeout(() => upgradedSendSignal(3, 900), 900);
+// проигнорировано так как следующий сигнал отменит предыдущий (1200 - 900 < 2000)
+setTimeout(() => upgradedSendSignal(4, 1200), 1200);
+// проигнорировано так как следующий сигнал отменит предыдущий (2300 - 1200 < 2000)
+setTimeout(() => upgradedSendSignal(5, 2300), 2300);
+// Сигнал отправлен так как следующий вызов не успеет отменить текущий: 4400-2300=2100 (2100 > 2000)
+setTimeout(() => upgradedSendSignal(6, 4400), 4400);
+// проигнорировано так как следующий сигнал отменит предыдущий (4500 - 4400 < 2000)
+setTimeout(() => upgradedSendSignal(7, 4500), 4500);
+// Сигнал будет отправлен, так как последний вызов debounce декоратора (спустя 4500 + 2000 = 6500) 6,5с
+
+setTimeout(() => {
+  console.log(upgradedSendSignal.count); // было выполнено 3 отправки сигнала
+  console.log(upgradedSendSignal.allCount); // было выполнено 6 вызовов декорированной функции
+}, 7000)
+
+
+
+
+
+
+
